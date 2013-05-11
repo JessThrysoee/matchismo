67786f14eade897f1f86975db2c94fa4c8f1da06
JessThrysoee <jess@thrysoee.dk>
2013-05-11 19:47:24 +0200

    back to basics. No segmented control or slider and controls are below cards now.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -15,8 +15,6 @@
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (weak, nonatomic) IBOutlet UILabel *resultLabel;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *matchCountControl;
-@property (weak, nonatomic) IBOutlet UISlider *slider;
 @property (nonatomic) int flipCount;
 @property (strong, nonatomic)IBOutletCollection(UIButton) NSArray * cardButtons;
 @property (strong, nonatomic) CardMatchingGame *game;
@@ -31,7 +29,7 @@
     if (!_game)
     {
         _game = [[CardMatchingGame alloc] initWithCardCount:self.cardButtons.count
-                                                 matchCount:self.matchCountControl.selectedSegmentIndex == 0 ? 2:3
+                                                 matchCount:2
                                                   usingDeck:[[PlayingCardDeck alloc] init]
                                                  flipResult:self.flipResult];
     }
@@ -54,7 +52,7 @@
 - (void)setFlipCount:(int)flipCount
 {
     _flipCount = flipCount;
-    self.flipsLabel.text = [NSString stringWithFormat:@"%3d", self.flipCount];
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
 }
 
 
@@ -62,7 +60,6 @@
 {
     NSUInteger index = [self.cardButtons indexOfObject:sender];
     
-    self.matchCountControl.enabled = NO;
     [self.game flipCardAtIndex:index];
     
     if ([self.game cardAtIndex:index].isFaceup)
@@ -101,23 +98,8 @@
         cardButton.alpha = card.isUnplayable ? 0.3 : 1.0;
     }
     
-    self.scoreLabel.text = [NSString stringWithFormat:@"%3d", self.game.score];
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", self.game.score];
     self.resultLabel.text = self.flipResult.lastResult;
-    
-    // slider
-    if (self.flipResult.count)
-    {
-        self.slider.maximumValue = self.flipResult.count;
-        self.slider.value = self.slider.maximumValue;
-        self.slider.enabled = YES;
-        self.resultLabel.alpha = 1;
-    }
-    else
-    {
-        self.slider.maximumValue = 0;
-        self.slider.value = 0;
-        self.slider.enabled = NO;
-    }
 }
 
 
@@ -133,32 +115,6 @@
 - (IBAction)deal
 {
     [self reset];
-    self.matchCountControl.enabled = YES;
 }
-
-
-- (IBAction)changeMatchCount:(UISegmentedControl *)sender
-{
-    [self reset];
-}
-
-
-- (IBAction)sliderChanged:(UISlider *)sender
-{
-    int value;
-    
-    value = lroundf(sender.value);
-    self.resultLabel.alpha = value < sender.maximumValue ? 0.5 : 1;
-    
-    if (value)
-    {
-        self.resultLabel.text = [self.flipResult resultAtIndex:value - 1];
-    }
-    else
-    {
-        self.resultLabel.text = @"";
-    }
-}
-
 
 @end
