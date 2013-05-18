e21966a0b205c5c8c5d940c9c848c61c9a2aa391
JessThrysoee <jess@thrysoee.dk>
2013-05-18 21:58:42 +0200

    New files

---
+++ Matchismo/SetCardCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  SetCardCollectionViewCell.m
+//  Matchismo
+//
+//  Created by Jess Thrysoee on 18/5-2013.
+//  Copyright (c) 2013 Jess Thrysoee. All rights reserved.
+//
+
+#import "SetCardCollectionViewCell.h"
+
+@implementation SetCardCollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ Matchismo/SetCardView.h
@@ -0,0 +1,13 @@
+//
+//  SetCardView.h
+//  Matchismo
+//
+//  Created by Jess Thrysoee on 18/5-2013.
+//  Copyright (c) 2013 Jess Thrysoee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetCardView : UICollectionViewCell
+
+@end
